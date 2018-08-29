051f3c9f821df482fedaaf2915276963ea319465
Noam Dori <TheNODO55@gmail.com>
2018-08-29 06:58:16 +0300

    added bracket matcher that will work on arrays

---
+++ src/main/java/ros/integrate/msg/ROSMsgBraceMatcher.java
@@ -0,0 +1,27 @@
+package ros.integrate.msg;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ros.integrate.msg.psi.ROSMsgTypes;
+
+public class ROSMsgBraceMatcher implements PairedBraceMatcher {
+    @NotNull
+    @Override
+    public BracePair[] getPairs() {
+        return new BracePair[]{new BracePair(ROSMsgTypes.LBRACKET,ROSMsgTypes.RBRACKET,false)};
+    }
+
+    @Override
+    public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType, @Nullable IElementType contextType) {
+        return contextType == ROSMsgTypes.CUSTOM_TYPE || contextType == ROSMsgTypes.KEYTYPE;
+    }
+
+    @Override
+    public int getCodeConstructStart(PsiFile file, int openingBraceOffset) {
+        return openingBraceOffset;
+    }
+}
