6e95bc331966ec66a2935b353d55c6ef4c893bfe
Noam Dori <TheNODO55@gmail.com>
2020-01-18 11:26:06 +0200

    autocompletion added for all other tag names (#19)
    Signed-off-by: Noam Dori <TheNODO55@gmail.com>

--- src/main/java/ros/integrate/pkg/xml/PackageXmlCompletionContributor.java
+++ src/main/java/ros/integrate/pkg/xml/PackageXmlCompletionContributor.java
@@ -54,8 +54,49 @@ public class PackageXmlCompletionContributor extends CompletionContributor {
     private void setCompletionsForTagName(@NotNull ROSPackageXml xmlFile, @NotNull CompletionResultSet resultSet,
                                           CompletionParameters parameters) {
         resultSet.runRemainingContributors(parameters, result -> {}); // this removes all other entries. Dangerous stuff.
+        if (xmlFile.getPkgName() == null) {
+            resultSet.addElement(LookupElementBuilder.create("name")
+                    .withInsertHandler((insertionContext, item) -> handleName(insertionContext, xmlFile)));
+        }
+        if (xmlFile.getVersion() == null) {
+            resultSet.addElement(LookupElementBuilder.create("version")
+                    .withInsertHandler((insertionContext, item) -> handleMoveToData(insertionContext,"version",false)));
+        }
+        if (xmlFile.getDescription() == null) {
+            resultSet.addElement(LookupElementBuilder.create("description")
+                    .withInsertHandler((insertionContext, item) -> handleDescription(insertionContext)));
+        }
         resultSet.addElement(LookupElementBuilder.create("url")
-                .withInsertHandler((insertionContext, item) -> handleUrl(insertionContext)));
+                .withInsertHandler((insertionContext, item) -> handleCompleteAttr(insertionContext, "url")));
+        resultSet.addElement(LookupElementBuilder.create("author")
+                .withInsertHandler((insertionContext, item) -> handleCompleteAttr(insertionContext, "author")));
+        resultSet.addElement(LookupElementBuilder.create("maintainer")
+                .withInsertHandler((insertionContext, item) -> handleMaintainer(insertionContext)));
+        resultSet.addElement(LookupElementBuilder.create("license")
+                .withInsertHandler((insertionContext, item) ->
+                        handleMoveToData(insertionContext, "license",false)));
+
+    }
+
+    private void handleDescription(@NotNull InsertionContext insertionContext) {
+        CaretModel model = insertionContext.getEditor().getCaretModel();
+        int offset = model.getOffset();
+        String text = insertionContext.getDocument().getText(new TextRange(offset, offset + 1));
+        if ("\r\n".contains(text)) {
+            insertionContext.getDocument().insertString(offset,">\n        \n    </description>");
+        }
+        model.getCurrentCaret().moveCaretRelatively(0,1,false, false);
+    }
+
+    private void handleName(@NotNull InsertionContext insertionContext, @NotNull ROSPackageXml xmlFile) {
+        CaretModel model = insertionContext.getEditor().getCaretModel();
+        int offset = model.getOffset();
+        String text = insertionContext.getDocument().getText(new TextRange(offset, offset + 1)),
+                insert = ">" + xmlFile.getPackage().getName() + "</name>";
+        if ("\r\n".contains(text)) {
+            insertionContext.getDocument().insertString(offset,insert);
+        }
+        model.getCurrentCaret().moveCaretRelatively(insert.length(),0,false, false);
     }
 
     private void setCompletionsForAttrName(@NotNull XmlTag tag, CompletionResultSet resultSet,
@@ -66,34 +107,46 @@ public class PackageXmlCompletionContributor extends CompletionContributor {
     private void addCompletionsForAttrValue(@NotNull XmlTag tag, CompletionResultSet resultSet) {
         if (tag.getName().equals("url")) {
             resultSet.addElement(LookupElementBuilder.create("website")
-                    .withInsertHandler((insertionContext, item) -> handleFinalAttribute(insertionContext, tag)));
+                    .withInsertHandler((insertionContext, item) -> handleMoveToData(insertionContext, tag.getName(), true)));
             resultSet.addElement(LookupElementBuilder.create("repository")
-                    .withInsertHandler((insertionContext, item) -> handleFinalAttribute(insertionContext, tag)));
+                    .withInsertHandler((insertionContext, item) -> handleMoveToData(insertionContext, tag.getName(), true)));
             resultSet.addElement(LookupElementBuilder.create("bugtracker")
-                    .withInsertHandler((insertionContext, item) -> handleFinalAttribute(insertionContext, tag)));
+                    .withInsertHandler((insertionContext, item) -> handleMoveToData(insertionContext, tag.getName(), true)));
         }
     }
 
-    private static void handleFinalAttribute(@NotNull InsertionContext insertionContext, XmlTag tag) {
+    private static void handleMoveToData(@NotNull InsertionContext insertionContext, String tagName, boolean inAttr) {
         CaretModel model = insertionContext.getEditor().getCaretModel();
-        model.getCurrentCaret().moveCaretRelatively(1,0,false, false);
+        if (inAttr) {
+            model.getCurrentCaret().moveCaretRelatively(1, 0, false, false);
+        }
         int offset = model.getOffset();
         String text = insertionContext.getDocument().getText(new TextRange(offset, offset + 1));
         if ("\r\n".contains(text)) {
-            insertionContext.getDocument().insertString(offset,"></" + tag.getName() + ">");
+            insertionContext.getDocument().insertString(offset,"></" + tagName + ">");
         }
         model.getCurrentCaret().moveCaretRelatively(1,0,false, false);
     }
 
-    private void handleUrl(@NotNull InsertionContext insertionContext) {
+    private void handleCompleteAttr(@NotNull InsertionContext insertionContext, String tagName) {
         CaretModel model = insertionContext.getEditor().getCaretModel();
         int offset = model.getOffset();
         String text = insertionContext.getDocument().getText(new TextRange(offset, offset + 1));
         if ("\r\n".contains(text)) {
-            insertionContext.getDocument().insertString(offset," ></url>");
+            insertionContext.getDocument().insertString(offset," ></" + tagName + ">");
         }
         model.getCurrentCaret().moveCaretRelatively(1,0,false, false);
         newCompletion(insertionContext.getProject(), insertionContext.getEditor());
+    }
+
+    private void handleMaintainer(@NotNull InsertionContext insertionContext) {
+        CaretModel model = insertionContext.getEditor().getCaretModel();
+        int offset = model.getOffset();
+        String text = insertionContext.getDocument().getText(new TextRange(offset, offset + 1));
+        if ("\r\n".contains(text)) {
+            insertionContext.getDocument().insertString(offset," email=\"\"></maintainer>");
+        }
+        model.getCurrentCaret().moveCaretRelatively(8,0,false, false);
     }
 
     private void newCompletion(Project project, Editor editor) {
