e314ae3d56865b295994fed89a865d0b2983ed8c
Noam Dori <TheNODO55@gmail.com>
2020-01-18 08:25:51 +0200

    removed old completion for tag names and added autocompletion (with chaining) for URL tag
    Signed-off-by: Noam Dori <TheNODO55@gmail.com>

--- src/main/java/ros/integrate/pkg/xml/PackageXmlCompletionContributor.java
+++ src/main/java/ros/integrate/pkg/xml/PackageXmlCompletionContributor.java
@@ -3,10 +3,14 @@ package ros.integrate.pkg.xml;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.lang.xml.XMLLanguage;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlElementType;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlToken;
@@ -24,7 +28,8 @@ public class PackageXmlCompletionContributor extends CompletionContributor {
                     public void addCompletions(@NotNull CompletionParameters parameters,
                                                @NotNull ProcessingContext context,
                                                @NotNull CompletionResultSet resultSet) {
-                        if (PackageXmlUtil.getWrapper(parameters.getOriginalFile()) == null) {
+                        ROSPackageXml xmlFile = PackageXmlUtil.getWrapper(parameters.getOriginalFile());
+                        if (xmlFile == null) {
                             return;
                         }
                         XmlToken element = (XmlToken) parameters.getPosition();
@@ -35,14 +40,27 @@ public class PackageXmlCompletionContributor extends CompletionContributor {
                         }
                         if (type.equals(XmlElementType.XML_ATTRIBUTE_VALUE_TOKEN)) {
                             addCompletionsForAttrValue(tag, resultSet);
+                        } else if (type.equals(XmlElementType.XML_NAME) && element.getParent() instanceof XmlAttribute) {
+                            setCompletionsForAttrName(tag, resultSet, parameters);
+                        } else if (type.equals(XmlElementType.XML_NAME)) {
+                            setCompletionsForTagName(xmlFile, resultSet, parameters);
                         }
-                        // XML_NAME: <[url] [type]="website">http://example.com</[url]>
-                        // make sure to split these into scenarios
-                        // XML_ATTRIBUTE_VALUE_TOKEN: <url type="[website]">http://example.com</url>
                         // XML_DATA_CHARACTERS: <url type="[website]">[http://example.com]</url>
-                        // these are also split by space-bars
+                        // these are also split by space-bars. could be useful for licenses
                     }
                 });
+    }
+
+    private void setCompletionsForTagName(@NotNull ROSPackageXml xmlFile, @NotNull CompletionResultSet resultSet,
+                                          CompletionParameters parameters) {
+        resultSet.runRemainingContributors(parameters, result -> {}); // this removes all other entries. Dangerous stuff.
+        resultSet.addElement(LookupElementBuilder.create("url")
+                .withInsertHandler((insertionContext, item) -> handleUrl(insertionContext)));
+    }
+
+    private void setCompletionsForAttrName(@NotNull XmlTag tag, CompletionResultSet resultSet,
+                                           CompletionParameters parameters) {
+
     }
 
     private void addCompletionsForAttrValue(@NotNull XmlTag tag, CompletionResultSet resultSet) {
@@ -65,5 +83,21 @@ public class PackageXmlCompletionContributor extends CompletionContributor {
             insertionContext.getDocument().insertString(offset,"></" + tag.getName() + ">");
         }
         model.getCurrentCaret().moveCaretRelatively(1,0,false, false);
+    }
+
+    private void handleUrl(@NotNull InsertionContext insertionContext) {
+        CaretModel model = insertionContext.getEditor().getCaretModel();
+        int offset = model.getOffset();
+        String text = insertionContext.getDocument().getText(new TextRange(offset, offset + 1));
+        if ("\r\n".contains(text)) {
+            insertionContext.getDocument().insertString(offset," ></url>");
+        }
+        model.getCurrentCaret().moveCaretRelatively(1,0,false, false);
+        newCompletion(insertionContext.getProject(), insertionContext.getEditor());
+    }
+
+    private void newCompletion(Project project, Editor editor) {
+        ApplicationManager.getApplication().invokeLater(() -> new CodeCompletionHandlerBase(CompletionType.BASIC, true, false, true)
+                .invokeCompletion(project, editor));
     }
 }
