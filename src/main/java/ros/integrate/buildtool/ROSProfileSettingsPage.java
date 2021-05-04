0d1f35edf84acf909758a5b0f981ac470b5d7fdc
Noam Dori <TheNODO55@gmail.com>
2021-05-04 09:58:31 +0300

    Detail form is now loaded on demand and per profile (#56) also added an explanation for when no profile is selected
    Signed-off-by: Noam Dori <TheNODO55@gmail.com>

--- src/main/java/ros/integrate/buildtool/ROSProfileSettingsPage.java
+++ src/main/java/ros/integrate/buildtool/ROSProfileSettingsPage.java
@@ -2,6 +2,7 @@ package ros.integrate.buildtool;
 
 import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.project.Project;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBTextField;
 import com.intellij.util.ui.FormBuilder;
@@ -11,6 +12,8 @@ import ros.integrate.buildtool.ui.SelectableListTable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * the user interface that allows the user to add,get, or modify the project's ROS buildtool configurations,
@@ -21,6 +24,8 @@ public class ROSProfileSettingsPage implements SearchableConfigurable {
     private final ROSProfiles data;
     private final SelectableListTable profileList;
     private final Project project;
+    private final Map<Integer, JPanel> profileForms = new HashMap<>();
+    private Integer selectedId = null;
 
     /**
      * construct a new settings page
@@ -61,8 +66,22 @@ public class ROSProfileSettingsPage implements SearchableConfigurable {
         listLayout.gridx = listLayout.gridy = 0;
         listLayout.fill = GridBagConstraints.VERTICAL;
         listLayout.ipadx = 80;
-        listLayout.anchor = GridBagConstraints.FIRST_LINE_START;
+        listLayout.anchor = GridBagConstraints.NORTHWEST;
         ret.add(profileList.getComponent(), listLayout);
+
+        GridBagConstraints labelLayout = new GridBagConstraints();
+        labelLayout.weightx = labelLayout.weighty = labelLayout.gridx = 1;
+        labelLayout.gridy = 0;
+        labelLayout.fill = GridBagConstraints.HORIZONTAL;
+        labelLayout.anchor = GridBagConstraints.CENTER;
+        labelLayout.insets.left = 10;
+        labelLayout.insets.top = 5;
+
+        JBLabel detailLabel = new JBLabel("Select profile to configure");
+        detailLabel.setForeground(JBColor.GRAY);
+        JPanel detailPanel = new JPanel();
+        detailPanel.add(detailLabel);
+        ret.add(detailPanel, labelLayout);
 
         GridBagConstraints formLayout = new GridBagConstraints();
         formLayout.weightx = formLayout.weighty = formLayout.gridx = 1;
@@ -71,11 +90,30 @@ public class ROSProfileSettingsPage implements SearchableConfigurable {
         formLayout.anchor = GridBagConstraints.NORTHWEST;
         formLayout.insets.left = 10;
         formLayout.insets.top = 5;
-        ret.add(new FormBuilder()
-                .addLabeledComponent(new JBLabel("Name:"), new JBTextField())
-                .addComponent(new JSeparator())
-                .getPanel(), formLayout);
 
+        profileList.getTableView().getSelectionModel().addListSelectionListener(e -> {
+            int newId = profileList.getTableView().getRow(e.getFirstIndex());
+            if (selectedId != null && selectedId.equals(newId)) {
+                return;
+            }
+            JPanel formToSelect = profileForms.get(newId);
+            if (formToSelect == null) {
+                formToSelect = new FormBuilder()
+                        .addLabeledComponent(new JBLabel("Name:"), new JBTextField())
+                        .addComponent(new JSeparator())
+                        .getPanel();
+                ret.add(formToSelect, formLayout);
+                profileForms.put(newId, formToSelect);
+                // TODO: 5/4/2021 read data and load into form
+            }
+            if (selectedId != null) {
+                profileForms.get(selectedId).setVisible(false);
+            } else {
+                detailLabel.setVisible(false);
+            }
+            selectedId = newId;
+            formToSelect.setVisible(true);
+        });
 
         return ret;
     }
